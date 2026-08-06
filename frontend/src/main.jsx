109fa7a954f8ffa9872d892e50615a615b2fb5af
Abdou Drb <abdou@example.com>
2026-08-06 15:30:05 +0100

    test supabase

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createClient } from "@supabase/supabase-js";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-const supabase = createClient(url, key);
+import { supabase } from "./utils/supabase";
 
 console.log(supabase);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
